05fa777ba6e1bc1dd3f8212fffda06daf2b9b5e6
Gustavo A Farias <farias.gustavo.a@gmail.com>
2025-01-14 09:47:03 -0300

    despliegue

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from bd.database import  engine, Base
 from routers.movie import routerMovie
 from routers.users import routerUser
 from routers.cifra import routerCifra
+import uvicorn
+import os
 
 # para ejecutar: uvicorn main:app --reload --port 4000
 
@@ -19,15 +21,13 @@ app.include_router(routerCifra)
 Base.metadata.create_all(bind=engine)
 
 
-
-
-
-
-
-
-
-
 @app.get('/', tags=['Inicio'])
 def read_root():
     return {'Hello':'World'}
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
+
+    
 
